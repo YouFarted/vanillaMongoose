ac3036c60bf34f0158a5905ba8010bc2a47fa93a
Andrew Olson <frrrkin@gmail.com>
2021-03-10 18:27:28 -0800

    enabled the use of __dirname which needed special treatment with ES6 modules

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 3001;
 
 let clientstaticpath = "";
 
+// needed for ES6 modules as configured in package.json where "type": "module"
+const __dirname = path.dirname(import.meta.url.substring(8));
+console.log(`__dirname = ${__dirname}`);
+
 if(process.env.NODE_ENV == "production") {
     clientstaticpath = path.join(__dirname, "..", "build", "clientbuild");
     // __dirname doesn't work with ES6 modules
